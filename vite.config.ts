a68a64c6a2b6f369f9d0502286d577d1956949b8
Jimmy <1064425721@qq.com>
2022-12-16 17:05:36 +0800

    build [vite] : 增加基础配置，增加多环境配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,44 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv, BuildOptions } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { join } from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ command, mode }) => {
+  //加载环境变量，打包路由路径
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [vue()],
+    base: env.VITE_BASEURL,
+    //添加快捷路径
+    resolve: {
+      alias: {
+        '@': join(__dirname, "src"),
+      }
+    },
+    //代理
+    server: {
+      host: '0.0.0.0',
+      https: false,
+      // https://github.com/http-party/node-http-proxy#options
+      proxy: {
+        '/api': {
+          target: `https://localhost:${env.VITE_PORT}/api/`,
+          changeOrigin: true,
+          secure: false,//验证 SSL 证书
+          rewrite: path => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+    // https://vitejs.dev/config/build-options.html#build-minify
+    build: {
+      minify: "terser",
+      terserOptions: {
+        compress: {
+          drop_console: command == 'build',
+          drop_debugger: command == 'build'
+        }
+      }
+    } as BuildOptions,
+  }
 })
