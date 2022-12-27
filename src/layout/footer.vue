c89e757b14a811a52e15d956ed87e4472320c790
Jimmy <jimmyrss1102@gmail.com>
2022-12-27 23:47:39 +0800

    feat [footer] : 添加页脚备案

--- src/layout/footer.vue
+++ src/layout/footer.vue
@@ -1,3 +1,25 @@
 <template>
-    footer
+    <div class="footer">
+        <a href="https://www.gsxt.gov.cn/" target="_blank">皖ICP备2021006489号-2</a>
+    </div>
 </template>
+
+<script setup lang="ts">
+
+</script>
+<style lang="scss" scoped>
+.footer {
+    padding: 28px;
+    background-color: rgb(88, 88, 88);
+    text-align: center;
+
+    a {
+        text-decoration: none;
+        color: white;
+
+        &:hover {
+            color: #000;
+        }
+    }
+}
+</style>
