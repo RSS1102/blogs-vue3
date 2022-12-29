649323d1a0df6cbe1dfaed9bb744852dcd44b289
Jimmy <jimmyrss1102@gmail.com>
2022-12-29 22:30:20 +0800

    feat! [progress] : 添加`progress`页面

--- src/layout/header.vue
+++ src/layout/header.vue
@@ -6,6 +6,7 @@ import { ref } from 'vue';
 import Menu from '@/components/Menu.vue';
 const menu = ref([
     { title: "首页", key: 'index', isDisabled: false },
-    { title: "博客", key: 'blog', isDisabled: true }
+    { title: "博客", key: 'blog', isDisabled: true },
+    { title: "工程", key: 'progress', isDisabled: true }
 ])
 </script>
--- src/router/menu.ts
+++ src/router/menu.ts
@@ -1,7 +1,10 @@
 import { RouteRecordRaw } from "vue-router";
 import Index from '@/pages/home/index.vue'
 import Blog from "@/pages/blogs/index.vue"
+import Progress from '@/pages/progress/index.vue'
+
 export const Menu: RouteRecordRaw[] = [
     { path: '/index', component: Index },
     { path: '/blog', component: Blog },
+    { path: '/progress', component: Progress },
 ]
